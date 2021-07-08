8bad049026ae9ae317317560d64f56d0d512ca21
Sebastien Boeuf <sebastien.boeuf@intel.com>
2021-07-08 12:11:32 +0200

    arm, arm64, x86: Remove v4.14 bindings
    This patch makes the bindings v4.20 the only available version for all
    architectures. Because of this unique possibility, the code is
    simplified, removing the specific Rust features to pick a specific
    bindings version.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- src/lib.rs
+++ src/lib.rs
@@ -17,14 +17,14 @@ extern crate versionize_derive;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod x86;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-pub use self::x86::bindings::*;
+pub use self::x86::*;
 
 #[cfg(target_arch = "aarch")]
 mod arm;
 #[cfg(target_arch = "aarch")]
-pub use self::arm::bindings::*;
+pub use self::arm::*;
 
 #[cfg(target_arch = "aarch64")]
 mod arm64;
 #[cfg(target_arch = "aarch64")]
-pub use self::arm64::bindings::*;
+pub use self::arm64::*;
